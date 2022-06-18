faadcfbbfba4be99527a862001a00360c1dc863d
Shubham Patidar <69835953+luckypatidar@users.noreply.github.com>
2022-06-18 21:50:59 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BRent/',
   plugins: [react(
     { fastRefresh: false }
-  )]
+  )],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
